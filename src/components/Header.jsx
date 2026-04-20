19ce07638b81b4337b9758414f5925e824f64021
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-04-20 14:49:24 +0100

    Add header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return(
+    <header className="bg-gray-900 text-white p-4 text-center">
+      <h1 className="text-2xl font-bold">RAD5 Tech Hub</h1>
+      <p className="text-sm mt-1">
+        Training the Next Generation of Developers
+      </p>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profilecard.jsx
@@ -0,0 +1,13 @@
+function ProfileCard() {
+  const name = "Chukwuemeka Obi";
+  const course = "Frontend Engineering";
+  const city = "Lagos";
+  const tuitionPaid = 150000;
+
+  return (
+    // display all four values here
+    // format tuitionPaid as ₦150,000 using .toLocaleString()
+  );
+}
+
+export default ProfileCard;
