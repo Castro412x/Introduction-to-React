c4f77f0f5eff2b5d70ef837b17c6632248f7380f
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-04-20 15:36:48 +0100

    Add footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-6">
+      <p>Built by Emmanuel Nonyerem</p>
+      <p className="text-sm mt-1">© 2025 RAD5 Tech Hub</p>
+    </footer>
+  );
+}
+
+export default Footer;
